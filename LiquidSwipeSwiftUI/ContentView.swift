3f3fd035468379b94591001d7487c042fd417f74
Mark Goldin <mgoldin@eastbanctech.com>
2019-08-22 17:49:00 +0700

    codestyle

--- LiquidSwipeSwiftUI/ContentView.swift
+++ LiquidSwipeSwiftUI/ContentView.swift
@@ -8,68 +8,6 @@
 
 import SwiftUI
 
-extension Color {
-    init(hex: Int, alpha: Double = 1) {
-        let components = (
-            R: Double((hex >> 16) & 0xff) / 255,
-            G: Double((hex >> 08) & 0xff) / 255,
-            B: Double((hex >> 00) & 0xff) / 255
-        )
-        self.init(
-            .sRGB,
-            red: components.R,
-            green: components.G,
-            blue: components.B,
-            opacity: alpha
-        )
-    }
-}
-
-class LiquidSwipeSettings {
-    static let shared = LiquidSwipeSettings()
-
-    let colors: [Color] = [
-        Color(hex: 0x0074D9),
-        Color(hex: 0x7FDBFF),
-        Color(hex: 0x39CCCC),
-        
-        Color(hex: 0x3D9970),
-        Color(hex: 0x2ECC40),
-        Color(hex: 0x01FF70),
-        
-        Color(hex: 0xFFDC00),
-        Color(hex: 0xFF851B),
-        Color(hex: 0xFF4136),
-        Color(hex: 0xF012BE),
-        
-        Color(hex: 0xB10DC9),
-        Color(hex: 0xAAAAAA),
-        Color(hex: 0xDDDDDD)
-    ].shuffled()
-    
-    var nextColor: Color {
-        if colorIndex < colors.count - 1 {
-            colorIndex += 1
-        } else {
-            colorIndex = 0
-        }
-        
-        return colors[colorIndex]
-    }
-    
-    private var colorIndex = -1
-    
-    var prevColor: Color {
-        if colorIndex > 0 {
-            colorIndex -= 1
-        } else {
-            colorIndex = colors.count - 1
-        }
-        
-        return colors[colorIndex]
-    }
-}
-
 struct ContentView: View {
     
     @State var backColor: Color = LiquidSwipeSettings.shared.nextColor
